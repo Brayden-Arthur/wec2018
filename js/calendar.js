10659aa81df5b897d69109e78f186c6e5d97fe0c
brayden-arthur <braydenarthur@gmail.com>
2018-01-12 15:22:10 -0800

    bs

--- js/calendar.js
+++ js/calendar.js
@@ -1,5 +1,11 @@
 $('#month_view').fullCalendar({
-  // methods here. see API
+      header: {
+				left: 'prev,next today',
+				center: 'title',
+				right: 'month,agendaWeek'
+			},
+      defaultView: 'month',
+      editable: true
 })
 
 $('#day_view').fullCalendar({
