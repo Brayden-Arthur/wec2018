05259f2d6ea1d8cce6f97081774da292198d9859
G <nthmorgan@gmail.com>
2018-01-12 17:56:13 -0800

    delete notes

--- js/notes.js
+++ js/notes.js
@@ -1,71 +1,66 @@
 // this is called after DOM parsing
-setup()
+const noteParent = document.getElementById('note_area')
 
-function note_click() {
-  var num_notes = document.querySelectorAll('.note').length;
-  var element = document.createElement("div");
-  var note = document.createElement("textarea");
-  note.setAttribute("id", "text" + (num_notes + 1));
-  element.appendChild(note);
-  element.className="note";
-  var close = document.createElement("span");
-  var close_text = document.createTextNode("\u2715");
-  close.setAttribute("class", "close");
-  close.setAttribute("name", "close");
-  close.setAttribute("onclick", "remove_note(this);");
-  close.appendChild(close_text);
-  element.appendChild(close);
-  var note_area = document.getElementById("note_area");
-  note_area.prepend(element);
+draw()
+
+function saveNotes() {
+  let notes = localStorage.getItem('notes')
+  if (notes == '' || notes == null) {
+    console.log('no notes to save')
+    return
+  }
+  console.log('saving notes')
+  notes = notes.split(',')
+  const textareas = noteParent.querySelectorAll('textarea')
+  let save = false
+  for (const area of textareas) {
+    const safeText = area.value.replace(/,/g, '%2C')
+    if (notes[area.id] !== safeText) {
+      notes[area.id] = safeText
+      save = true
+    }
+  }
+  if (save) {
+    localStorage.setItem('notes', notes)
+  }
 }
+setInterval(saveNotes, 1000)
 
-function local_store() {
-  var num_notes = document.querySelectorAll('.note').length;
-  for(i = 1; i <= num_notes; i++){
-    var note = document.getElementById("text" + i).value;
-    localStorage.setItem("note" + i, note);
-    localStorage.setItem("num_notes",num_notes)
+function draw() {
+  noteParent.innerHTML = ''
+  let notes = localStorage.getItem('notes')
+  if (!notes) {
+    console.log('no notes to draw')
+    return
+  }
+  notes = notes.split(',')
+  for (const noteIndex in notes) {
+    const note = notes[noteIndex]
+
+    const element = document.createElement('div')
+    const textarea = document.createElement('textarea')
+    textarea.id = noteIndex
+    textarea.value = note.replace('%2C', ',')
+    element.appendChild(textarea)
+    element.className = 'note'
+    const close = document.createElement('span')
+    close.innerText = '\u2715'
+    close.class = 'close'
+    close.addEventListener('click', event => {
+      notes.splice(noteIndex, 1)
+      console.log('deleting')
+      console.log(notes)
+      localStorage.setItem('notes', notes)
+      draw()
+    })
+    element.appendChild(close)
+    noteParent.prepend(element)
   }
 }
 
-function remove_note(close_button) {
-  var parent = close_button.parentElement;
-  task_name = parent.innerText.replace("✕","");
-  localStorage.removeItem(parent.firstChild.id.replace('text','note'));
-  parent.style.display = 'none';
-  localStorage.setItem("task_list",task_list.toString());
-  var num_of_notes = parseInt(localStorage.getItem('num_notes'));
-  num_of_notes -= 1;
-  localStorage.setItem('num_notes',num_of_notes);
-}
-
-function setup() {
-  var num_notes = localStorage.getItem("num_notes");
-  if(num_notes){
-    if(num_notes == 1){
-      document.getElementById("text1").value = localStorage.getItem("note1")
-    }
-    else{
-      for(i = 2; i <= num_notes; i++){
-        var element = document.createElement("div");
-        var note = document.createElement("textarea");
-
-        note.setAttribute("id", "text" + i);
-        element.appendChild(note);
-        element.className="note";
-        var close = document.createElement("span");
-        var close_text = document.createTextNode("\u2715");
-        close.setAttribute("class", "close");
-        close.setAttribute("name", "close");
-        close.setAttribute("onclick", "remove_note(this);");
-        close.appendChild(close_text);
-        element.appendChild(close);
-
-        document.getElementById("note_area").prepend(element);
-      }
-      for(i = 1; i <= num_notes; i++){
-        document.getElementById("text" + i).value = localStorage.getItem("note" + i)
-      }
-    }
-  }
-}
+document.getElementById('add_note').addEventListener('click', event => {
+  let tempNotes = localStorage.getItem('notes') || ''
+  tempNotes += ',New Note!'
+  localStorage.setItem('notes', tempNotes)
+  draw()
+})
